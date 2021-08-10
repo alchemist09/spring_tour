5369b826b9bf6af082fe904cc7151af418006145
Luke Olali <mugapedia@gmail.com>
2021-08-10 23:49:14 +0300

    defines method that instructs Spring to create a JdbcProductDao

--- src/main/java/com/crashcourse/cfg/AppConfig1.java
+++ src/main/java/com/crashcourse/cfg/AppConfig1.java
@@ -1,6 +1,7 @@
 package com.crashcourse.cfg;
 
 import com.crashcourse.dao.DummyProductDao;
+import com.crashcourse.dao.JdbcProductDao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class AppConfig1 {
   @Bean
   public DummyProductDao dummyDao() {
     return new DummyProductDao();
+  }
+
+  @Bean
+  public JdbcProductDao jdbcDao() {
+    return new JdbcProductDao();
   }
  }
