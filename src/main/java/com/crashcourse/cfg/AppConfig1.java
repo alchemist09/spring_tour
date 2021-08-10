6563ea69bbb7dd36b4b5953020b66f8bc128373a
Luke Olali <mugapedia@gmail.com>
2021-08-10 23:48:07 +0300

    defines method that tell spring to return a DummyProductDao bean

--- src/main/java/com/crashcourse/cfg/AppConfig1.java
+++ src/main/java/com/crashcourse/cfg/AppConfig1.java
@@ -1,5 +1,14 @@
 package com.crashcourse.cfg;
 
+import com.crashcourse.dao.DummyProductDao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig1 {
-  
-}
+  @Bean
+  public DummyProductDao dummyDao() {
+    return new DummyProductDao();
+  }
+ }
