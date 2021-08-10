fbe519473dcf810ca66cd030ba6f09b9bd6fd859
Luke Olali <mugapedia@gmail.com>
2021-08-11 00:22:46 +0300

    creates an instance of the spring container and passes it a configuration class

--- src/main/java/com/crashcourse/programs/P01_GetProductCount.java
+++ src/main/java/com/crashcourse/programs/P01_GetProductCount.java
@@ -1,5 +1,6 @@
 package com.crashcourse.programs;
 
+import com.crashcourse.cfg.AppConfig1;
 import com.crashcourse.dao.ProductDao;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -17,5 +18,8 @@ public class P01_GetProductCount
 
         // declare a variable representing the Spring container
         AnnotationConfigApplicationContext ctx;
+
+        // create object of spring container
+        ctx = new AnnotationConfigApplicationContext(AppConfig1.class);
     }
 }
