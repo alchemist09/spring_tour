285ceaf839f685571ec6f587fbfea6bbb5828b06
Luke Olali <mugapedia@gmail.com>
2021-08-11 00:17:26 +0300

    declares the dependency the application relies on. Renames the package declaration line

---
+++ src/main/java/com/crashcourse/programs/P01_GetProductCount.java
@@ -0,0 +1,16 @@
+package com.crashcourse.programs;
+
+import com.crashcourse.dao.ProductDao;
+
+/**
+ * Hello world!
+ *
+ */
+public class P01_GetProductCount 
+{
+    public static void main( String[] args )
+    {
+        // declare our dependency
+        ProductDao dao;
+    }
+}
