a29a97accd7408aff6cb86d1b8ac06f14be6c1f3
Luke Olali <mugapedia@gmail.com>
2021-08-10 23:40:54 +0300

    add docstrings to count() method

--- src/main/java/com/crashcourse/dao/JdbcProductDao.java
+++ src/main/java/com/crashcourse/dao/JdbcProductDao.java
@@ -1,6 +1,9 @@
 package com.crashcourse.dao;
 
 public class JdbcProductDao implements ProductDao {
+  /**
+   * Count the number of records in database
+   */
   public long count() {
     return 100;
   }
