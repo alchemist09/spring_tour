966c296f0d2d093bf5fab05ee4fe2e63f5f8dc3a
Luke Olali <mugapedia@gmail.com>
2021-08-15 15:05:00 +0300

    defines instance variables for JdbcProductDao

--- src/main/java/com/crashcourse/dao/JdbcProductDao.java
+++ src/main/java/com/crashcourse/dao/JdbcProductDao.java
@@ -1,6 +1,11 @@
 package com.crashcourse.dao;
 
 public class JdbcProductDao implements ProductDao {
+  private String driverClassName;
+  private String url;
+  private String username;
+  private String password;
+
   /**
    * Count the number of records in database
    */
