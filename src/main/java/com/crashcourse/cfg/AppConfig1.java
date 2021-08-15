853c9df11e2ebfa4fb928aab2f007e02b5dd70fa
Luke Olali <mugapedia@gmail.com>
2021-08-16 00:35:48 +0300

    reads in values from the jdbc.properties file into AppConfig1 instance variables

--- src/main/java/com/crashcourse/cfg/AppConfig1.java
+++ src/main/java/com/crashcourse/cfg/AppConfig1.java
@@ -4,17 +4,24 @@ import com.crashcourse.dao.DummyProductDao;
 import com.crashcourse.dao.JdbcProductDao;
 import com.crashcourse.dao.OdbcDao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@PropertySource("classpath:jdbc.properties")
 public class AppConfig1 {
 
+  @Value("${jdbc.driver}")
   private String driverClassname;
+  @Value("${jdbc.url}")
   private String url;
+  @Value("jdbc.user")
   private String username;
+  @Value("${jdbc.password}")
   private String password;
 
   public AppConfig1() {
