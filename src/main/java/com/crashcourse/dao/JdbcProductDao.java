70ca1a5b8addb84668609ab41e9a5756dbaeb49d
Luke Olali <mugapedia@gmail.com>
2021-08-15 23:38:15 +0300

    creates a connection to the database

--- src/main/java/com/crashcourse/dao/JdbcProductDao.java
+++ src/main/java/com/crashcourse/dao/JdbcProductDao.java
@@ -1,5 +1,9 @@
 package com.crashcourse.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +16,15 @@ public class JdbcProductDao implements ProductDao {
   private String url;
   private String username;
   private String password;
+
+  /**
+   * Create a database connection
+   * @return
+   */
+  private Connection createConnection() throws ClassNotFoundException, SQLException {
+    Class.forName(driverClassName);
+    return DriverManager.getConnection(url, username, password);
+  }
 
   /**
    * Count the number of records in database
