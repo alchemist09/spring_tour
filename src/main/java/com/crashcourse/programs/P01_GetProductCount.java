93226ac4ecb6df2dcf8fc7f1bc2b1f3b7a385d66
Luke Olali <mugapedia@gmail.com>
2021-08-18 17:25:35 +0300

    tests program with the configuration from AppConfig2.java file

--- src/main/java/com/crashcourse/programs/P01_GetProductCount.java
+++ src/main/java/com/crashcourse/programs/P01_GetProductCount.java
@@ -1,6 +1,6 @@
 package com.crashcourse.programs;
 
-import com.crashcourse.cfg.AppConfig1;
+import com.crashcourse.cfg.AppConfig2;
 import com.crashcourse.dao.ProductDao;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,17 +20,17 @@ public class P01_GetProductCount
         AnnotationConfigApplicationContext ctx;
 
         // create object of spring container
-        ctx = new AnnotationConfigApplicationContext(AppConfig1.class);
+        ctx = new AnnotationConfigApplicationContext(AppConfig2.class);
         System.out.println("=================================================");
 
-        dao = ctx.getBean("odbcDao", ProductDao.class);
-        ProductDao dao2 = ctx.getBean("odbcDao", ProductDao.class);
+        // dao = ctx.getBean("odbcDao", ProductDao.class);
+        // ProductDao dao2 = ctx.getBean("odbcDao", ProductDao.class);
         ProductDao dao3 = ctx.getBean("jdbcDao", ProductDao.class);
 
-        System.out.println("dao2 == dao is " + (dao2 == dao));
+        // System.out.println("dao2 == dao is " + (dao2 == dao));
 
-        System.out.println("dao is an instance of: " + dao.getClass().getName());
-        System.out.println("there are " + dao.count() + " products.");
+        // System.out.println("dao is an instance of: " + dao.getClass().getName());
+        // System.out.println("there are " + dao.count() + " products.");
         System.out.println("********");
         System.out.println("No. of products from database: " + dao3.count());
 
