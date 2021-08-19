4c51a9277871c27bb44849d7d31cd4b84ef908ee
Luke Olali <mugapedia@gmail.com>
2021-08-19 23:46:57 +0300

    removes the ComponentScan annotation import

--- src/main/java/com/crashcourse/cfg/AppConfig4.java
+++ src/main/java/com/crashcourse/cfg/AppConfig4.java
@@ -7,7 +7,6 @@ import com.crashcourse.dao.JdbcProductDao;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
