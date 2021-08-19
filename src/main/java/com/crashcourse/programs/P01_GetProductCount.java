da404c8e15fe0579b2cd3be5994f1701b0177749
Luke Olali <mugapedia@gmail.com>
2021-08-19 18:27:57 +0300

    removes unused variable declarations

--- src/main/java/com/crashcourse/programs/P01_GetProductCount.java
+++ src/main/java/com/crashcourse/programs/P01_GetProductCount.java
@@ -2,7 +2,6 @@ package com.crashcourse.programs;
 
 import com.crashcourse.cfg.AppConfig3;
 import com.crashcourse.dao.ProductDao;
-import org.h2.jdbc.JdbcConnection;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,7 +15,6 @@ public class P01_GetProductCount
     {
         // declare our dependency
         ProductDao dao;
-        JdbcConnection cntn;
 
         // declare a variable representing the Spring container
         AnnotationConfigApplicationContext ctx;
@@ -25,14 +23,12 @@ public class P01_GetProductCount
         ctx = new AnnotationConfigApplicationContext(AppConfig3.class);
         System.out.println("=================================================");
 
-        // cntn = ctx.getBean("connection", JdbcConnection.class);
         // ProductDao dao2 = ctx.getBean("odbcDao", ProductDao.class);
         dao = ctx.getBean("jdbcDao", ProductDao.class);
         ProductDao dao2 = ctx.getBean("jdbcDao", ProductDao.class);
 
         System.out.println("dao2 == dao is " + (dao2 == dao));
-
-        // System.out.println("cntn is an instance of: " + cntn.getClass().getName());
+        
         // System.out.println("there are " + dao.count() + " products.");
         System.out.println("********");
         System.out.println("No. of products from database: " + dao.count());
