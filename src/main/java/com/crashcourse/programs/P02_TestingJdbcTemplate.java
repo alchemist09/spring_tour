180942151b917bf2513b8f4097f9008cc560bccb
Luke Olali <mugapedia@gmail.com>
2021-08-20 00:02:53 +0300

    creates the spring application context

--- src/main/java/com/crashcourse/programs/P02_TestingJdbcTemplate.java
+++ src/main/java/com/crashcourse/programs/P02_TestingJdbcTemplate.java
@@ -1,7 +1,13 @@
 package com.crashcourse.programs;
 
+import com.crashcourse.cfg.AppConfig4;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class P02_TestingJdbcTemplate {
   public static void main(String[] args) {
-    
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
+
+    ctx.close();
   }
 }
