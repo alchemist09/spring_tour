0694dafec4a516e4e54db6ddf5d3cee93d6f58e5
Luke Olali <mugapedia@gmail.com>
2021-08-19 18:03:36 +0300

    adds initial configuration information to AppConfig3 file

---
+++ src/main/java/com/crashcourse/cfg/AppConfig3.java
@@ -0,0 +1,28 @@
+package com.crashcourse.cfg;
+
+import com.crashcourse.dao.JdbcProductDao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+public class AppConfig3 {
+
+  @Value("${jdbc.driver}")
+  private String driverClassname;
+  @Value("${jdbc.url}")
+  private String url;
+  @Value("${jdbc.user}")
+  private String username;
+  @Value("${jdbc.password}")
+  private String password;
+
+  
+  @Bean
+  public JdbcProductDao jdbcDao() {
+    return new JdbcProductDao();
+  } 
+ }
