33cae609d3ae7ee7cfa8f20a7f8c127e6d4b52e0
Luke Olali <mugapedia@gmail.com>
2021-08-19 18:26:48 +0300

    changes the configuration file to AppConfig3.java. Runs the application using the new config file

--- src/main/java/com/crashcourse/programs/P01_GetProductCount.java
+++ src/main/java/com/crashcourse/programs/P01_GetProductCount.java
@@ -1,6 +1,6 @@
 package com.crashcourse.programs;
 
-import com.crashcourse.cfg.AppConfig2;
+import com.crashcourse.cfg.AppConfig3;
 import com.crashcourse.dao.ProductDao;
 import org.h2.jdbc.JdbcConnection;
 
@@ -22,17 +22,17 @@ public class P01_GetProductCount
         AnnotationConfigApplicationContext ctx;
 
         // create object of spring container
-        ctx = new AnnotationConfigApplicationContext(AppConfig2.class);
+        ctx = new AnnotationConfigApplicationContext(AppConfig3.class);
         System.out.println("=================================================");
 
-        cntn = ctx.getBean("connection", JdbcConnection.class);
+        // cntn = ctx.getBean("connection", JdbcConnection.class);
         // ProductDao dao2 = ctx.getBean("odbcDao", ProductDao.class);
         dao = ctx.getBean("jdbcDao", ProductDao.class);
         ProductDao dao2 = ctx.getBean("jdbcDao", ProductDao.class);
 
         System.out.println("dao2 == dao is " + (dao2 == dao));
 
-        System.out.println("cntn is an instance of: " + cntn.getClass().getName());
+        // System.out.println("cntn is an instance of: " + cntn.getClass().getName());
         // System.out.println("there are " + dao.count() + " products.");
         System.out.println("********");
         System.out.println("No. of products from database: " + dao.count());
