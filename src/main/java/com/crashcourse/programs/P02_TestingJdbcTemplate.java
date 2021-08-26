4dd490d7276a0cb2f6764142a8f048e11ec9faae
Luke Olali <mugapedia@gmail.com>
2021-08-26 22:45:38 +0300

    defines method for adding shippers to database

--- src/main/java/com/crashcourse/programs/P02_TestingJdbcTemplate.java
+++ src/main/java/com/crashcourse/programs/P02_TestingJdbcTemplate.java
@@ -3,11 +3,24 @@ package com.crashcourse.programs;
 import com.crashcourse.cfg.AppConfig4;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class P02_TestingJdbcTemplate {
+
+  static JdbcTemplate template;
+
   public static void main(String[] args) {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
 
     ctx.close();
+  }
+
+  static void addShipper() {
+    int shipper_id = 4;
+    String company_name = "Alibaba";
+    String phone = "+1 328561135";
+    String query = "INSERT INTO shippers values (?, ?, ?)";
+    template.update(query, shipper_id, company_name, phone);
+    System.out.println("New shipper added to database");
   }
 }
