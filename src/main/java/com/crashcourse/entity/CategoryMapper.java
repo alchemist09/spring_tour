e737e19d58736f3df44d3b4e3a45068f4c7d9afe
Luke Olali <mugapedia@gmail.com>
2021-09-02 01:37:38 +0300

    implements mapRow() method for CategoryMapper class

--- src/main/java/com/crashcourse/entity/CategoryMapper.java
+++ src/main/java/com/crashcourse/entity/CategoryMapper.java
@@ -1,12 +1,17 @@
 package com.crashcourse.entity;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
 public class CategoryMapper implements RowMapper<Category> {
-  public Category mapRow(ResultSet rs, int rowNum) {
+  public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
     Category cg = new Category();
+    cg.setCategory_id(rs.getInt("category_id"));
+    cg.setCategory_name(rs.getString("category_name"));
+    cg.setDescription(rs.getString("description"));
+    cg.setPictture(rs.getString("picture"));
     return cg;
   }
 }
