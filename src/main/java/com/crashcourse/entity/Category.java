d2a364bcb958faef282bb2866c41c3514a36980c
Luke Olali <mugapedia@gmail.com>
2021-09-02 01:59:09 +0300

    creates a constructor which does not require any args

--- src/main/java/com/crashcourse/entity/Category.java
+++ src/main/java/com/crashcourse/entity/Category.java
@@ -1,10 +1,12 @@
 package com.crashcourse.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Category {
   private int category_id;
   private String category_name;
