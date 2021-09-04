e14030176b3a62576347a9f365501b1850a094f8
Luke Olali <mugapedia@gmail.com>
2021-09-04 23:22:23 +0300

    defines constructor, getters and setter

--- src/main/java/com/crashcourse/entity/Region.java
+++ src/main/java/com/crashcourse/entity/Region.java
@@ -1,5 +1,12 @@
 package com.crashcourse.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Region {
   private int region_id;
   private String region_description;
