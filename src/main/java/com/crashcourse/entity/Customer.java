6b41455df40d5f385ecabfe33ca3f21201ca42ed
Luke Olali <mugapedia@gmail.com>
2021-09-06 23:58:46 +0300

    defines Customer class

---
+++ src/main/java/com/crashcourse/entity/Customer.java
@@ -0,0 +1,15 @@
+package com.crashcourse.entity;
+
+public class Customer {
+  private int customer_id;
+  private String company_name;
+  private String contact_name;
+  private String contact_title;
+  private String address;
+  private String city;
+  private String region;
+  private String postal_code;
+  private String country;
+  private String phone;
+  private String fax;
+}
