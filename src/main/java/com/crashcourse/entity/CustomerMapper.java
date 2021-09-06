6dd393ae7403bc95f35406a4bb6fed4b613cc3b0
Luke Olali <mugapedia@gmail.com>
2021-09-07 00:12:03 +0300

    implenments the mapRow() method from the RowMapper interface

--- src/main/java/com/crashcourse/entity/CustomerMapper.java
+++ src/main/java/com/crashcourse/entity/CustomerMapper.java
@@ -8,6 +8,17 @@ import org.springframework.jdbc.core.RowMapper;
 public class CustomerMapper implements RowMapper<Customer> {
   public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
     Customer c = new Customer();
+    c.setCustomer_id(rs.getInt("custoner_id"));
+    c.setCompany_name(rs.getString("company_name"));
+    c.setContact_name(rs.getString("contact_name"));
+    c.setContact_title(rs.getString("contact_title"));
+    c.setAddress(rs.getString("address"));
+    c.setCity(rs.getString("city"));
+    c.setRegion(rs.getString("region"));
+    c.setPostal_code(rs.getString("postal_code"));
+    c.setCountry(rs.getString("country"));
+    c.setPhone(rs.getString("phone"));
+    c.setFax(rs.getString("fax"));
     return c;
   }  
 }
