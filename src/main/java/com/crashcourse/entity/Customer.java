1a4487a2a0895309bf3e87efda1798b5e21c432d
Luke Olali <mugapedia@gmail.com>
2021-09-06 23:59:27 +0300

    adds getters, setters and constructor

--- src/main/java/com/crashcourse/entity/Customer.java
+++ src/main/java/com/crashcourse/entity/Customer.java
@@ -1,5 +1,12 @@
 package com.crashcourse.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer {
   private int customer_id;
   private String company_name;
