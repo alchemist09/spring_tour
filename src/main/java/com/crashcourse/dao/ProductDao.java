ce240d46311ddcd1029e0198a3e1f4c1257cfe1b
Luke Olali <mugapedia@gmail.com>
2021-09-09 23:49:57 +0300

    declares methods used to perform CRUD operations in the ProductDao interface

--- src/main/java/com/crashcourse/dao/ProductDao.java
+++ src/main/java/com/crashcourse/dao/ProductDao.java
@@ -1,5 +1,14 @@
 package com.crashcourse.dao;
 
+import com.crashcourse.entity.Product;
+
 public interface ProductDao {
+  // CRUD OPERATIONS
+  public void addProduct(Product product) throws DaoException;
+  public void updateProduct(Product product) throws DaoException;
+  public Product getProduct(Integer product_id) throws DaoException;
+  public void deleteProduct(Integer product_id) throws DaoException;
+
+  //  QUERIES
   public long count();
 }
