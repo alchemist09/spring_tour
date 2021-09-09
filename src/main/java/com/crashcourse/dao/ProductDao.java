1efde3eb5cce0a6a8b4dbc7fa9e292e2068d5058
Luke Olali <mugapedia@gmail.com>
2021-09-10 00:06:19 +0300

    adds default implementation to methods declared in ProductDao interface to prevent breaking existing implementing classes

--- src/main/java/com/crashcourse/dao/ProductDao.java
+++ src/main/java/com/crashcourse/dao/ProductDao.java
@@ -6,17 +6,46 @@ import com.crashcourse.entity.Product;
 
 public interface ProductDao {
   // CRUD OPERATIONS
-  public void addProduct(Product product) throws DaoException;
-  public void updateProduct(Product product) throws DaoException;
-  public Product getProduct(Integer product_id) throws DaoException;
-  public void deleteProduct(Integer product_id) throws DaoException;
+  public default void addProduct(Product product) throws DaoException {
+    throw new DaoException();
+  }
+
+  public default void updateProduct(Product product) throws DaoException {
+    throw new DaoException();
+  }
+
+  public default Product getProduct(Integer product_id) throws DaoException {
+    throw new DaoException();
+  }
+
+  public default void deleteProduct(Integer product_id) throws DaoException {
+    throw new DaoException();
+  }
 
   //  QUERIES
   public long count();
-  public List<Product> getAllProducts() throws DaoException;
-  public List<Product> getProductsByPriceRange(Double min_price, Double max_price) throws DaoException;
-  public List<Product> getProductsInCategory(Integer category_id) throws DaoException;
-  public List<Product> getProductsNotInStock() throws DaoException;
-  public List<Product> getProductsOnOrder() throws DaoException;
-  public List<Product> getDiscontinuedProducts() throws DaoException;
+
+  public default List<Product> getAllProducts() throws DaoException {
+    throw new DaoException();
+  }
+
+  public default List<Product> getProductsByPriceRange(Double min_price, Double max_price) throws DaoException {
+    throw new DaoException();
+  }
+
+  public default List<Product> getProductsInCategory(Integer category_id) throws DaoException {
+    throw new DaoException();
+  }
+
+  public default List<Product> getProductsNotInStock() throws DaoException {
+    throw new DaoException();
+  }
+
+  public default List<Product> getProductsOnOrder() throws DaoException {
+    throw new DaoException();
+  }
+
+  public default List<Product> getDiscontinuedProducts() throws DaoException {
+    throw new DaoException();
+  }
 }
