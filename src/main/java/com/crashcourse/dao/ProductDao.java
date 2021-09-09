61cd169ecbea1b7d84a5c10bdf81185d2d7fd983
Luke Olali <mugapedia@gmail.com>
2021-09-10 00:21:58 +0300

    specifiy default detail message for exception being thrown

--- src/main/java/com/crashcourse/dao/ProductDao.java
+++ src/main/java/com/crashcourse/dao/ProductDao.java
@@ -7,45 +7,45 @@ import com.crashcourse.entity.Product;
 public interface ProductDao {
   // CRUD OPERATIONS
   public default void addProduct(Product product) throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default void updateProduct(Product product) throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default Product getProduct(Integer product_id) throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default void deleteProduct(Integer product_id) throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   //  QUERIES
   public long count();
 
   public default List<Product> getAllProducts() throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default List<Product> getProductsByPriceRange(Double min_price, Double max_price) throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default List<Product> getProductsInCategory(Integer category_id) throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default List<Product> getProductsNotInStock() throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default List<Product> getProductsOnOrder() throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 
   public default List<Product> getDiscontinuedProducts() throws DaoException {
-    throw new DaoException();
+    throw new DaoException("Method not implemented");
   }
 }
