b95a9292a18e3d3356b56c4bbd13d95aca1bc6af
Luke Olali <mugapedia@gmail.com>
2021-09-09 23:19:27 +0300

    creates the Product class

---
+++ src/main/java/com/crashcourse/entity/Product.java
@@ -0,0 +1,14 @@
+package com.crashcourse.entity;
+
+public class Product {
+  private int product_id;
+  private String product_name;
+  private int supplier_id;
+  private int category_id;
+  private String quantity_per_unit;
+  private double unit_price;
+  private int units_in_stock;
+  private int units_on_order;
+  private int reorder_level;
+  private int discontinued;
+}
