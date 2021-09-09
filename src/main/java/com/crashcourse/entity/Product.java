cae0eb74bfd21eacbfcd1b3aad4a04b2223c6633
Luke Olali <mugapedia@gmail.com>
2021-09-09 23:20:13 +0300

    defines getters and setters

--- src/main/java/com/crashcourse/entity/Product.java
+++ src/main/java/com/crashcourse/entity/Product.java
@@ -1,5 +1,12 @@
 package com.crashcourse.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
   private int product_id;
   private String product_name;
