04ef013d0ee85c379e90621d7c4033fa4f7d5aff
Luke Olali <mugapedia@gmail.com>
2021-09-11 23:58:48 +0300

    defines getter and setters

--- src/main/java/com/crashcourse/entity/Employee.java
+++ src/main/java/com/crashcourse/entity/Employee.java
@@ -1,5 +1,12 @@
 package com.crashcourse.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Employee {
   private int employee_id;
   private String last_name;
