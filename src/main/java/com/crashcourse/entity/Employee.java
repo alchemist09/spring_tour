01cc6cc5046da7e831760a3586c4d138c1c92e5a
Luke Olali <mugapedia@gmail.com>
2021-09-12 01:06:15 +0300

    adds additional fields

--- src/main/java/com/crashcourse/entity/Employee.java
+++ src/main/java/com/crashcourse/entity/Employee.java
@@ -13,4 +13,16 @@ public class Employee {
   private String first_name;
   private String title;
   private String title_of_courtesy;
+  private String birth_date;
+  private String hire_date;
+  private String address;
+  private String city;
+  private String region;
+  private int postal_code;
+  private String country;
+  private String home_phone;
+  private int extension;
+  private String photo;
+  private String notes;
+  private int reports_to;
 }
