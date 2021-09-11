0d553bbefad877fd848ae46818ae6235ba9e4213
Luke Olali <mugapedia@gmail.com>
2021-09-11 23:57:00 +0300

    creates Employee class

---
+++ src/main/java/com/crashcourse/entity/Employee.java
@@ -0,0 +1,9 @@
+package com.crashcourse.entity;
+
+public class Employee {
+  private int employee_id;
+  private String last_name;
+  private String first_name;
+  private String title;
+  private String title_of_courtesy;
+}
