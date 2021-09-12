5b4154469e551f9033c822739e2b9639130448fc
Luke Olali <mugapedia@gmail.com>
2021-09-12 19:06:40 +0300

    adds ToString() annotation to the Product entity

--- src/main/java/com/crashcourse/entity/Product.java
+++ src/main/java/com/crashcourse/entity/Product.java
@@ -3,7 +3,9 @@ package com.crashcourse.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter
 @Setter
 @NoArgsConstructor
