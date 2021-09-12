63c3459b1ddc78bc605704fdb4beac8e4f254cd5
Luke Olali <mugapedia@gmail.com>
2021-09-12 19:07:18 +0300

    tests getProduct() method

--- src/main/java/com/crashcourse/programs/P03_TestingJdbcTemplateProductDao.java
+++ src/main/java/com/crashcourse/programs/P03_TestingJdbcTemplateProductDao.java
@@ -1,13 +1,21 @@
 package com.crashcourse.programs;
 
 import com.crashcourse.cfg.AppConfig4;
+import com.crashcourse.dao.DaoException;
+import com.crashcourse.dao.ProductDao;
+import com.crashcourse.entity.Product;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class P03_TestingJdbcTemplateProductDao {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws DaoException {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
 
+    ProductDao dao = ctx.getBean("jtDao", ProductDao.class);
+
+    Product p1 = dao.getProduct(1);
+    System.out.println(p1);
+    
     ctx.close();
   }
 }
