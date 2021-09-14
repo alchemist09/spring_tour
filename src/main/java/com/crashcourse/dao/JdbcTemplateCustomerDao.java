4e0f37ace093a53192ecd3e00a426c4d3dbe5fb9
Luke Olali <mugapedia@gmail.com>
2021-09-15 01:33:41 +0300

    implements method for adding a customer to database

--- src/main/java/com/crashcourse/dao/JdbcTemplateCustomerDao.java
+++ src/main/java/com/crashcourse/dao/JdbcTemplateCustomerDao.java
@@ -1,5 +1,7 @@
 package com.crashcourse.dao;
 
+import com.crashcourse.entity.Customer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,11 @@ public class JdbcTemplateCustomerDao implements CustomerDao {
 
   @Autowired(required = false)
   private JdbcTemplate template;
+
+  @Override
+  public void addCustomer(Customer c) throws DaoException {
+    String sql = "INSERT INTO customers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    template.update(sql, c.getCustomer_id(), c.getCompany_name(), c.getContact_name(), c.getContact_title(), c.getAddress(), 
+                         c.getCity(), c.getRegion(), c.getPostal_code(), c.getCountry(), c.getPhone(), c.getFax());
+  }
 }
