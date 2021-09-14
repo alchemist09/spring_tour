64e5d83eec95d2fc1fe2275f050c4c8fa629d6e6
Luke Olali <mugapedia@gmail.com>
2021-09-15 01:25:10 +0300

    autowires JdbcTemplate to the repository class to provide database operations

--- src/main/java/com/crashcourse/dao/JdbcTemplateCustomerDao.java
+++ src/main/java/com/crashcourse/dao/JdbcTemplateCustomerDao.java
@@ -1,8 +1,12 @@
 package com.crashcourse.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository("customerDao")
 public class JdbcTemplateCustomerDao implements CustomerDao {
-  
+
+  @Autowired(required = false)
+  private JdbcTemplate template;
 }
