30bf64eb6cbbe85f12afce51f6595eaa385d5e6d
Luke Olali <mugapedia@gmail.com>
2021-09-16 00:54:41 +0300

    tests adding customer to dataabase

--- src/main/java/com/crashcourse/programs/P05_TestingJdbcTemplateCustomerDao.java
+++ src/main/java/com/crashcourse/programs/P05_TestingJdbcTemplateCustomerDao.java
@@ -13,13 +13,32 @@ public class P05_TestingJdbcTemplateCustomerDao {
 
     CustomerDao dao = ctx.getBean("customerDao", CustomerDao.class);
 
+    // looking up a customer
     Customer c1 = dao.getCustomer("BSBEV");
     System.out.println(c1);
 
+    // updating customer details
     Customer c2 = dao.getCustomer("ALFKI");
     c2.setRegion("BC");
     dao.updateCustomer(c2);
-    System.out.println("Customer details updated");
+    System.out.println("\r\nCustomer details updated");
+
+    // adding a customer
+    Customer c3 = new Customer();
+    c3.setCustomer_id("XXXLL");
+    c3.setCompany_name("Pivotal Labs");
+    c3.setContact_name("John Doe");
+    c3.setContact_title("Agent");
+    c3.setAddress("123 Milky Way");
+    c3.setCity("Casablanca");
+    c3.setRegion("SF");
+    c3.setPostal_code("85890");
+    c3.setCountry("Finland");
+    c3.setPhone("49-163-2299");
+    c3.setFax("1349-15388");
+
+    dao.addCustomer(c3);
+    System.out.println("\r\nAdded new customer to database");
 
     ctx.close();
   }
