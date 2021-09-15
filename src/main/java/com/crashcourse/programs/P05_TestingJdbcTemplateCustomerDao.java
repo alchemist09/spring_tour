0bb728e2e891e6a3fc6849e43bdeaaf3355f509e
Luke Olali <mugapedia@gmail.com>
2021-09-16 00:23:55 +0300

    tests looking up a customer by their ID

--- src/main/java/com/crashcourse/programs/P05_TestingJdbcTemplateCustomerDao.java
+++ src/main/java/com/crashcourse/programs/P05_TestingJdbcTemplateCustomerDao.java
@@ -1,12 +1,20 @@
 package com.crashcourse.programs;
 
 import com.crashcourse.cfg.AppConfig4;
+import com.crashcourse.dao.CustomerDao;
+import com.crashcourse.dao.DaoException;
+import com.crashcourse.entity.Customer;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class P05_TestingJdbcTemplateCustomerDao {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws DaoException {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
+
+    CustomerDao dao = ctx.getBean("customerDao", CustomerDao.class);
+
+    Customer c1 = dao.getCustomer("BSBEV");
+    System.out.println(c1);
 
     ctx.close();
   }
