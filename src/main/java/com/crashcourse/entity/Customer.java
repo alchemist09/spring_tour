6c401a0fb7ef5aeb3e7d9aed05fb7542e9291c00
Luke Olali <mugapedia@gmail.com>
2021-09-16 23:48:33 +0300

    specifies Customer class is an entity to be managed by hibernate

--- src/main/java/com/crashcourse/entity/Customer.java
+++ src/main/java/com/crashcourse/entity/Customer.java
@@ -1,15 +1,22 @@
 package com.crashcourse.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Entity
+@Table(name = "customers")
 @ToString
 @Getter
 @Setter
 @NoArgsConstructor
 public class Customer {
+  @Id
   private String customer_id;
   private String company_name;
   private String contact_name;
