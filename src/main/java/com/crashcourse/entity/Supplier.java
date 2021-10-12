045d43b0ea79cb5c53e2873b7da55981ec6a9221
Luke Olali <mugapedia@gmail.com>
2021-10-12 23:55:18 +0300

    specifies the Supplier class as an entity in hibernate. Marks the primary key field

--- src/main/java/com/crashcourse/entity/Supplier.java
+++ src/main/java/com/crashcourse/entity/Supplier.java
@@ -1,13 +1,20 @@
 package com.crashcourse.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "suppliers")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Supplier {
+  @Id
   private int supplier_id;
   private String company_name;
   private String contact_name;
