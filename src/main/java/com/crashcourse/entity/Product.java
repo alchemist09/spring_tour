b0041130a1bcb4cd8f89614732d684aa7cc34843
Luke Olali <mugapedia@gmail.com>
2021-10-13 02:03:11 +0300

    specifies the associations the Product entity has with the Supplier and Categories entities

--- src/main/java/com/crashcourse/entity/Product.java
+++ src/main/java/com/crashcourse/entity/Product.java
@@ -1,7 +1,10 @@
 package com.crashcourse.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -19,8 +22,21 @@ public class Product {
   @Id
   private int product_id;
   private String product_name;
+
+  @Column(insertable = false, updatable = false)
   private int supplier_id;
+
+  @ManyToOne
+  @JoinColumn(name = "supplier_id")
+  private Supplier supplier;
+
+  @Column(insertable = false, updatable = false)
   private int category_id;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id")
+  private Category category;
+
   private String quantity_per_unit;
   private double unit_price;
   private int units_in_stock;
