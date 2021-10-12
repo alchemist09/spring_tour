703095c5c5c363e2a07a1ebc231f06fb1cf4610b
Luke Olali <mugapedia@gmail.com>
2021-10-12 23:52:15 +0300

    creates a supplier entity class

---
+++ src/main/java/com/crashcourse/entity/Supplier.java
@@ -0,0 +1,17 @@
+package com.crashcourse.entity;
+
+public class Supplier {
+  private int supplier_id;
+  private String company_name;
+  private String contact_name;
+  private String contact_title;
+  private String address;
+  private String city;
+  private String region;
+  private String postal_code;
+  private String country;
+  private String phone;
+  private String fax;
+  private String home_page;
+  
+}
