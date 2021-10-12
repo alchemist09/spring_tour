e9b11d2a9027c8782665aca89309770cabfb6429
Luke Olali <mugapedia@gmail.com>
2021-10-12 23:59:53 +0300

    specifies the Product class as an entity in hibernate

--- src/main/java/com/crashcourse/entity/Product.java
+++ src/main/java/com/crashcourse/entity/Product.java
@@ -1,15 +1,22 @@
 package com.crashcourse.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Entity
+@Table(name = "products")
 @ToString
 @Getter
 @Setter
 @NoArgsConstructor
 public class Product {
+  @Id
   private int product_id;
   private String product_name;
   private int supplier_id;
