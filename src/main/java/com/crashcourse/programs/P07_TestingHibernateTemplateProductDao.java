1e4b74776233ce6a200438df6e34e5bd5725d279
Luke Olali <mugapedia@gmail.com>
2021-10-20 01:45:16 +0300

    tests fetching all products

--- src/main/java/com/crashcourse/programs/P07_TestingHibernateTemplateProductDao.java
+++ src/main/java/com/crashcourse/programs/P07_TestingHibernateTemplateProductDao.java
@@ -1,12 +1,21 @@
 package com.crashcourse.programs;
 
+import java.util.List;
+
 import com.crashcourse.cfg.AppConfig4;
+import com.crashcourse.dao.DaoException;
+import com.crashcourse.dao.ProductDao;
+import com.crashcourse.entity.Product;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class P07_TestingHibernateTemplateProductDao {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws DaoException {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
+
+    ProductDao dao = ctx.getBean("htDao", ProductDao.class);
+    List<Product> product_list = dao.getAllProducts();
+    System.out.println("There are " + product_list.size() + " products in the database");
 
     ctx.close();
   }
