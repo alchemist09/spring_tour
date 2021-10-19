4a238adb37d2064dcafb52ad45987b4fdfcadf43
Luke Olali <mugapedia@gmail.com>
2021-10-19 23:58:04 +0300

    autowires the hibernateTemplate bean

--- src/main/java/com/crashcourse/dao/HibernateTemplateProductDao.java
+++ src/main/java/com/crashcourse/dao/HibernateTemplateProductDao.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import com.crashcourse.entity.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository("htDao")
 public class HibernateTemplateProductDao implements ProductDao {
+
+  @Autowired(required = false)
+  private HibernateTemplate template;
 
   @Override
   public void addProduct(Product product) throws DaoException {
