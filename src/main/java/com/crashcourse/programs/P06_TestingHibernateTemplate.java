ff3f3d651be1a015224800065192f73e526cc498
Luke Olali <mugapedia@gmail.com>
2021-10-19 23:20:40 +0300

    fetch a persistent instance of Category type that matches ID specified

--- src/main/java/com/crashcourse/programs/P06_TestingHibernateTemplate.java
+++ src/main/java/com/crashcourse/programs/P06_TestingHibernateTemplate.java
@@ -1,6 +1,7 @@
 package com.crashcourse.programs;
 
 import com.crashcourse.cfg.AppConfig4;
+import com.crashcourse.entity.Category;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -10,6 +11,9 @@ public class P06_TestingHibernateTemplate {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
 
     HibernateTemplate ht = ctx.getBean(HibernateTemplate.class);
+
+    Category c1 = ht.get(Category.class, 1);
+    System.out.println(c1);
 
     ctx.close();
   }
