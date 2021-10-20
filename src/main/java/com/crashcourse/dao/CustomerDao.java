b8898b26022dd57dc0b151e994bffa9be4b08a05
Luke Olali <mugapedia@gmail.com>
2021-10-21 00:06:42 +0300

    fixes typo in method parameter names

--- src/main/java/com/crashcourse/dao/CustomerDao.java
+++ src/main/java/com/crashcourse/dao/CustomerDao.java
@@ -6,11 +6,11 @@ import com.crashcourse.entity.Customer;
 
 public interface CustomerDao {
   // CRUD OPERATIONS ON CUSTOMER ENTITY
-  public default void addCustomer(Customer custoemr) throws DaoException {
+  public default void addCustomer(Customer customer) throws DaoException {
     throw new DaoException("implemented in DAO class");
   }
 
-  public default void updateCustomer(Customer custoemr) throws DaoException {
+  public default void updateCustomer(Customer customer) throws DaoException {
     throw new DaoException("implemented in DAO class");
   }
 
