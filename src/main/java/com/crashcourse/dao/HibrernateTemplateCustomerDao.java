88bd0b06187b858e6ba40cd7b706946441f4dc05
Luke Olali <mugapedia@gmail.com>
2021-10-20 23:52:36 +0300

    removes default implementations of methods from ProductDao. CustomerDao should be used

--- src/main/java/com/crashcourse/dao/HibrernateTemplateCustomerDao.java
+++ src/main/java/com/crashcourse/dao/HibrernateTemplateCustomerDao.java
@@ -1,79 +1,10 @@
 package com.crashcourse.dao;
 
-import java.util.List;
-
-import com.crashcourse.entity.Product;
-
 import org.springframework.stereotype.Repository;
 
 @Repository("htCustDao")
-public class HibrernateTemplateCustomerDao implements ProductDao {
+public class HibrernateTemplateCustomerDao implements CustomerDao {
 
-  @Override
-  public void addProduct(Product product) throws DaoException {
-    // TODO Auto-generated method stub
-    ProductDao.super.addProduct(product);
-  }
-
-  @Override
-  public long count() throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.count();
-  }
-
-  @Override
-  public void deleteProduct(Integer product_id) throws DaoException {
-    // TODO Auto-generated method stub
-    ProductDao.super.deleteProduct(product_id);
-  }
-
-  @Override
-  public List<Product> getAllProducts() throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getAllProducts();
-  }
-
-  @Override
-  public List<Product> getDiscontinuedProducts() throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getDiscontinuedProducts();
-  }
-
-  @Override
-  public Product getProduct(Integer product_id) throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getProduct(product_id);
-  }
-
-  @Override
-  public List<Product> getProductsByPriceRange(Double min_price, Double max_price) throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getProductsByPriceRange(min_price, max_price);
-  }
-
-  @Override
-  public List<Product> getProductsInCategory(Integer category_id) throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getProductsInCategory(category_id);
-  }
-
-  @Override
-  public List<Product> getProductsNotInStock() throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getProductsNotInStock();
-  }
-
-  @Override
-  public List<Product> getProductsOnOrder() throws DaoException {
-    // TODO Auto-generated method stub
-    return ProductDao.super.getProductsOnOrder();
-  }
-
-  @Override
-  public void updateProduct(Product product) throws DaoException {
-    // TODO Auto-generated method stub
-    ProductDao.super.updateProduct(product);
-  }
   
 }
 
