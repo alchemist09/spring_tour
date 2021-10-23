c12d3a1067a974ab9f6be3169faceb2a72c3eeee
Luke Olali <mugapedia@gmail.com>
2021-10-23 23:59:19 +0300

    tests fetching all customers

--- src/main/java/com/crashcourse/programs/P08_TestingHibernateTemplateCustomerDao.java
+++ src/main/java/com/crashcourse/programs/P08_TestingHibernateTemplateCustomerDao.java
@@ -1,12 +1,22 @@
 package com.crashcourse.programs;
 
+import java.util.List;
+
 import com.crashcourse.cfg.AppConfig4;
+import com.crashcourse.dao.CustomerDao;
+import com.crashcourse.dao.DaoException;
+import com.crashcourse.entity.Customer;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class P08_TestingHibernateTemplateCustomerDao {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws DaoException {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig4.class);
+
+    CustomerDao dao = ctx.getBean("htCustDao", CustomerDao.class);
+    List<Customer> customer_list = dao.getAllCustomers();
+    System.out.println("There are " + customer_list.size() + " customers in database");
+
 
     ctx.close();
   }
