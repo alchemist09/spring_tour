305c303a375a0454501d3ad540df69553abbff97
Luke Olali <mugapedia@gmail.com>
2021-10-26 00:11:53 +0300

    defines an advice that is executed by aspect before target method execution

--- src/main/java/com/crashcourse/aspect/MyAspect.java
+++ src/main/java/com/crashcourse/aspect/MyAspect.java
@@ -1,10 +1,18 @@
 package com.crashcourse.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component
 public class MyAspect {
   public MyAspect() {
     System.out.println("My Aspect instantitated!!!");
+  }
+
+  @Before("execution(* com.crashcourse.dao.ProductDao.count())")
+  public void logBefore() {
+    System.out.println("Logging before target method execution");
   }
 }
