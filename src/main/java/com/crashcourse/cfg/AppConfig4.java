c645745e9a66698404d2fcdea49b553b4a8a82e8
Luke Olali <mugapedia@gmail.com>
2021-10-26 00:29:57 +0300

    enable support for components annotated with @Aspect

--- src/main/java/com/crashcourse/cfg/AppConfig4.java
+++ src/main/java/com/crashcourse/cfg/AppConfig4.java
@@ -15,11 +15,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+@EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"com.crashcourse.dao", "com.crashcourse.aspect"})
 @Configuration
 @PropertySource("classpath:jdbc.properties")
