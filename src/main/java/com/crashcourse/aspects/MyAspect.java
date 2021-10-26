e35fcaeb7d7f80e879507fa4766b317d33791d4e
Luke Olali <mugapedia@gmail.com>
2021-10-27 00:36:49 +0300

    defines an adviser for swapping inputs passed to a method

--- src/main/java/com/crashcourse/aspects/MyAspect.java
+++ src/main/java/com/crashcourse/aspects/MyAspect.java
@@ -3,6 +3,8 @@ package com.crashcourse.aspects;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -18,5 +20,19 @@ public class MyAspect {
   public void logBefore(JoinPoint jp) {
     System.out.println("\r\nBefore executing " + jp.getSignature().getName());
     System.out.println("Method arguments are: " + Arrays.toString(jp.getArgs()));
+  }
+
+  @Around("execution(* com.crashcourse.dao.ProductDao.get*(Double, Double))")
+  public Object swapInputs(ProceedingJoinPoint pjp) {
+    Object[] args = pjp.getArgs();
+
+    Double min = (Double)args[0];
+    Double max = (Double)args[1];
+
+    if(min > max) {
+      args = new Object[] {max, min};
+    }
+
+    return pjp.proceed(args);
   }
 }
